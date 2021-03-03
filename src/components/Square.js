4735668b02c3e574cf6583a3220c0930fb0caea6
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-03 05:42:03 +0530

    feat: adding interactive component

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './Square.css';
 
 class Square extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={function() { alert('click'); }}>
-       {this.props.value}
-      </button>
+        <button className="square"
+            onClick={() => this.setState({ value: 'X' })}>
+            {this.state.value}
+        </button>
     );
   }
 }
